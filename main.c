ec6fb32beeb977253d3998a5eacb56d57e408672
Classihappiness <happyjhoy23@gmail.com>
2022-05-17 14:35:35 -0700

    simple_shell

--- main.c
+++ main.c
@@ -1,10 +1,40 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int ac, char **av){
-    int i = 0;
-
-    for (i = 0; i < ac; i++)
-    printf("%s\n", av[i]);
-    return (0);
-}
+#include "main.h" 
+  
+ /** 
+  * sig_handler - checks if Ctrl C is pressed 
+  * @sig_num: int 
+  */ 
+ void sig_handler(int sig_num) 
+ { 
+         if (sig_num == SIGINT) 
+         { 
+                 _puts("\n#cisfun$ "); 
+         } 
+ } 
+  
+ /** 
+ * _EOF - handles the End of File 
+ * @len: return value of getline function 
+ * @buff: buffer 
+  */ 
+ void _EOF(int len, char *buff) 
+ { 
+         (void)buff; 
+         if (len == -1) 
+         { 
+                 if (isatty(STDIN_FILENO)) 
+                 { 
+                         _puts("\n"); 
+                         free(buff); 
+                 } 
+                 exit(0); 
+         } 
+ } 
+ /** 
+   * _isatty - verif if terminal 
+   */ 
+  
+ void _isatty(void) 
+ { 
+         if (isatty(STDIN_FILENO)) 
+                 _puts("#cisfun$ ");}
